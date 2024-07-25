d20a37b49fbfd017f61e6dc3782fe71a3b43e4d4
nay3on <nayeon0425s@gmail.com>
2024-07-25 21:39:04 +0900

    📝feat : 홈 페이지 구현

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../css/Home.css';
+import logo from '../assets/logo.svg';
 
 const Home = () => {
 
     return (
-      <div>
-        Home
+      <div className="home-container">
+        <div className="background" />
+        <div className="filter" />
+          <div className="circle" id="circle1" />
+          <div className="circle" id="circle2" />
+          <div className="circle" id="circle3" />
+          <div className="circle" id="circle4" />
+          <div className="circle" id="circle5" />
+          <div className="logo-container">
+            <img src={logo} alt="App Logo" className="app-logo" />
+          </div>
+          <div className="app-intro">
+            <p>동아리 관리의 모든 것</p>
+            <p>크루 유니버셜 클럽</p>
+          </div>
+          <div className="button-container">
+            <button className="btn sign-in">로그인</button>
+            <button className="btn sign-up">회원가입</button>
+          </div>
       </div>
     );
 }

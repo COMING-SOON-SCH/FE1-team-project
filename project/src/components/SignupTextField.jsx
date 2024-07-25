3c3a9aaabd220259386a4ed830071240518f06c9
conconcc <lesj9217@gmail.com>
2024-07-25 20:58:54 +1000

    📝 feat : 마이페이지, 클럽페이지 기능 추가 및 코드 작성

--- project/src/components/SignupTextField.jsx
+++ project/src/components/SignupTextField.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
+import { Grid, TextField, InputAdornment, IconButton, Box, FormControl } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
-
-export function SignupTextField({ texttype }) {
+export function SignupTextField({ texttype, value, onChange }) {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6}>
@@ -14,25 +13,74 @@ export function SignupTextField({ texttype }) {
           fullWidth
           id={texttype}
           label={texttype}
+          value={value}
+          onChange={onChange}
           autoFocus
         />
       </Grid>
     </Grid>
   );
 }
-export function PasswordField({ texttype }) {
+
+export function PasswordField({ texttype, value, onChange }) {
+  const isPasswordField = texttype.toLowerCase().includes("password");
+  <Box
+      component="form"
+      noValidate
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: { sm: '1fr 1fr' },
+        gap: 2,
+      }}
+    >
+      <FormControl variant="standard">
+        <inputlabel shrink htmlFor="bootstrap-input">
+          Bootstrap
+        </inputlabel>
+      </FormControl>
+    </Box>
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6}>
         <TextField
-          autoComplete={texttype}
-          name={texttype}
+          margin="normal"
           required
           fullWidth
-          id={texttype}
+          name={texttype}
           label={texttype}
-          autoFocus
+          type={isPasswordField ? "password" : "text"}
+          id={texttype}
+          autoComplete={isPasswordField ? 'current-password' : 'off'}
+          value={value}
+          onChange={onChange}
         />
+      </Grid>
+    </Grid>
+  );
+}
+
+export function SearchField({ label, value, onChange }) {
+  const handleSearchClick = () => {
+    alert("아직 구현중인 기능입니다.");
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <TextField
+      label={label}
+      value={value}
+      onChange={onChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton>
+              <SearchIcon onclick={handleSearchClick}/>
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
       </Grid>
     </Grid>
   );

aa4525ad0321ab4b9b58f38768e87bf8f6d905cb
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-07-28 13:35:19 +0900

    📰 docs : firebaseConfig 파일 수정
    사용량 초과로 인한 프로젝트 변경

--- project/src/firebaseConfig.js
+++ project/src/firebaseConfig.js
@@ -9,13 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
-  authDomain: "clubproject-3a751.firebaseapp.com",
-  projectId: "clubproject-3a751",
-  storageBucket: "clubproject-3a751.appspot.com",
-  messagingSenderId: "46325601848",
-  appId: "1:46325601848:web:913a7e6ba82bd0f4b9b8cd",
-  measurementId: "G-E0BSBMZRK2"
+  authDomain: "clubproject-ad699.firebaseapp.com",
+  projectId: "clubproject-ad699",
+  storageBucket: "clubproject-ad699.appspot.com",
+  messagingSenderId: "887959208594",
+  appId: "1:887959208594:web:3f035d92d43c17f3d99c4b",
+  measurementId: "G-2QKNK6467V"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

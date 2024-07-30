acd6573189014c83b55f7b5024678db068d38e2d
Yusi <yusi4781@naver.com>
2024-07-30 18:07:18 +0900

    🔨refactor : MUI Drawer로 Sidebar 구현
    기존 Sidebar를 삭제하고 MUI Drawer로 Sidebar를 구현했습니다.

--- project/src/pages/Main.jsx
+++ project/src/pages/Main.jsx
@@ -2,22 +2,28 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import CalendarComponent from "../components/Calendar";
-import SideBar from "../components/Sidebar";
 import sidebarpicture from '../assets/sidebar.png';
+import { Box, Drawer, Slide } from "@mui/material";
+import SidebarItem from "../components/SidebarItem";
 
 export default function Main() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
     <MainContainer>
-      <SidebarButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+      <SidebarButton onClick={() => setIsSidebarOpen(true)} >
         <ButtonImage src={sidebarpicture} alt='button' />
       </SidebarButton>
       <CalendarComponent />
       <Announcement />
-      {isSidebarOpen && (
-        <SideBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-      )}
+      <Drawer
+        open={isSidebarOpen}
+        onClose={() => setIsSidebarOpen(!isSidebarOpen)}
+        TransitionComponent={Slide}
+        transitionDuration={{ enter: 300, exit: 300 }}
+      >
+        <SidebarItem isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+      </Drawer>
     </MainContainer>
   );
 }
@@ -28,11 +34,10 @@ const MainContainer = styled.div`
   height: 140vh;
 `;
 
-const SidebarButton = styled.button`
-  width: 50px;
-  height: 50px;
-  margin: 10px;
-  margin-bottom: 0;
+const SidebarButton = styled(Box)`
+  width: 40px;
+  height: 40px;
+  padding: 10px 0px 0px 20px;
   background: none;
   border: none;
 `;

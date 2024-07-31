4bf7a957b696072534152ac2ef3651ba5f8db1fc
nay3on <nayeon0425s@gmail.com>
2024-07-31 19:53:30 +0900

    🔨refactor : 홈 화면, 로그인 화면 배경 관련 코드 수정

--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -3,10 +3,7 @@ import logo from '../assets/logo.svg';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Container from '../components/Container';
 import Background from '../components/Background';
-import BackgroundFilter from '../components/BackgroundFilter';
-import BackgroundCircle from '../components/BackgroundCircle';
 import Button from '../components/Button';
 
 const LogoContainer = styled.div`
@@ -23,7 +20,6 @@ const LogoContainer = styled.div`
   font-size: 24px;
   font-weight: bold;
   border: solid 1px #000;
-  position: relative;
   &::before, &::after {
     position: absolute;
     background: inherit;
@@ -74,14 +70,7 @@ const Home = () => {
   };
 
     return (
-      <Container>
-        <Background />
-        <BackgroundFilter />
-          <BackgroundCircle width="110px" height="110px" left="20%" top="85%" background="linear-gradient(#83b2ff, #67caff)" />
-          <BackgroundCircle width="150px" height="150px" left="12%" top="40%" background="linear-gradient(#00fbff, #7961ff)" />
-          <BackgroundCircle width="180px" height="180px" left="46%" top="65%" background="linear-gradient(#00ffd5, #00d9ff)" />
-          <BackgroundCircle width="170px" height="170px" left="25%" top="5%" background="linear-gradient(#4EFFF8, #57FFD7)" />
-          <BackgroundCircle width="140px" height="140px" left="55%" top="20%" background="linear-gradient(#4EFFF8, #69a2fe)" />
+      <Background>
           <LogoContainer>
             <AppLogo />
           </LogoContainer>
@@ -93,8 +82,8 @@ const Home = () => {
           <Button className="sign-in" onClick={onClickSignIn}>로그인</Button>
             <Link to="/signup"><Button className="sign-up">회원가입</Button></Link>
           </ButtonContainer>
-      </Container>
+      </Background>
     );
-}
+};
   
 export default Home;
--- project/src/pages/SignIn.jsx
+++ project/src/pages/SignIn.jsx
@@ -2,11 +2,7 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/SignIn.css';
 import TextField from '@mui/material/TextField';
-import styled from 'styled-components';
-import Container from '../components/Container';
 import Background from '../components/Background';
-import BackgroundFilter from '../components/BackgroundFilter';
-import BackgroundCircle from '../components/BackgroundCircle';
 import Button from '../components/Button';
 import BackButton from '../components/BackButton';
 
@@ -72,21 +68,14 @@ const SignIn = () => {
   };
 
     return (
-        <Container>
-          <Background />
-          <BackgroundFilter />
-            <BackgroundCircle width="110px" height="110px" left="20%" top="85%" background="linear-gradient(#83b2ff, #67caff)" />
-            <BackgroundCircle width="150px" height="150px" left="12%" top="40%" background="linear-gradient(#00fbff, #7961ff)" />
-            <BackgroundCircle width="180px" height="180px" left="46%" top="65%" background="linear-gradient(#00ffd5, #00d9ff)" />
-            <BackgroundCircle width="170px" height="170px" left="25%" top="5%" background="linear-gradient(#4EFFF8, #57FFD7)" />
-            <BackgroundCircle width="140px" height="140px" left="55%" top="20%" background="linear-gradient(#4EFFF8, #69a2fe)" />
-            <BackButton />
-            <OutlinedTextField userId={userId} setUserId={setUserId} password={password} setPassword={setPassword} />
-            <Button className="btn sign-in" onClick={handleSignIn}>로그인</Button>
+        <Background>
+          <BackButton />
+          <OutlinedTextField userId={userId} setUserId={setUserId} password={password} setPassword={setPassword} />
+          <Button className="btn sign-in" onClick={handleSignIn}>로그인</Button>
           <div className="forgot-password" onClick={onClickForgotPassword}>
             비밀번호를 잊으셨나요?
           </div>
-        </Container>
+        </Background>
       );
 };
 

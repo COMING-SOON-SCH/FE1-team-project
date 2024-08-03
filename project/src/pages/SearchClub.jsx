a1df5fa46106236b6787629c8a90fe49e9714e33
Yusi <yusi4781@naver.com>
2024-08-03 16:35:05 +0900

    💄style : 동아리 찾기 페이지의  Sidebar 버튼 중복 해제

--- project/src/pages/SearchClub.jsx
+++ project/src/pages/SearchClub.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Background from '../components/Background';
-import SideBarButton from '../components/SideBarButton';
 import SearchBar from '../components/SearchBar';
 import ContentContainer from '../components/ContentContainer';
 import SearchItemContainer from '../components/SearchItemContainer';
@@ -8,7 +7,6 @@ import SearchItemContainer from '../components/SearchItemContainer';
 const SearchClub = () => {
   return (
     <Background>
-    <SideBarButton />
     <SearchBar />
     <ContentContainer>
         <SearchItemContainer />

feda16af7eb248439ad26943a97fb650393ac616
Yusi <yusi4781@naver.com>
2024-08-03 16:34:03 +0900

    ✨feat : Sidebar 목록 수정 및 클릭 시 페이지 이동 기능 구현

--- project/src/components/SidebarItem.jsx
+++ project/src/components/SidebarItem.jsx
@@ -1,31 +1,72 @@
-import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
+import { useNavigate } from "react-router-dom";
+import { ContactPage, Logout } from "@mui/icons-material";
+import styled from "styled-components";
 
 export default function SidebarItem({ isSidebarOpen, setIsSidebarOpen }) {
+  const navigate = useNavigate();
+  const topItems = [
+    { text: '홈', url: 'main', icon: HomeIcon },
+    { text: '동아리 찾기', url: 'search-club', icon: SearchIcon },
+    { text: '동아리 만들기', url: 'makeclub', icon: CreateNewFolderIcon }
+  ];
+  const bottomItems = [
+    { text: '마이 페이지', url: 'mypage', icon: ContactPage },
+    { text: '로그아웃', url: null, icon: Logout }
+  ];
+
+  const handleItemClick = (item) => {
+    if (item.url) {
+      navigate(`./${item.url}`);
+    } else if (item.text === '로그아웃') {
+      // 여기에 로그아웃 로직을 구현합니다.
+      console.log('로그아웃 처리');
+      // 예: logout();
+      // 로그아웃 후 홈 페이지로 리디렉션
+      navigate('/');
+    }
+    setIsSidebarOpen(false);
+  };
+
   return (
-    <Box sx={{ width: 250 }} role="presentation" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+    <Box
+      sx={{
+        width: 250,
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+      role="presentation"
+    >
+      <LogoBox>
+
+      </LogoBox>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {topItems.map((item) => (
+          <ListItem key={item.text} disablePadding onClick={() => handleItemClick(item)}>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <item.icon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
-      <Divider />
+      <Box sx={{ flexGrow: 1 }} />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {bottomItems.map((item) => (
+          <ListItem key={item.text} disablePadding onClick={() => handleItemClick(item)}>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                <item.icon />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -33,3 +74,10 @@ export default function SidebarItem({ isSidebarOpen, setIsSidebarOpen }) {
     </Box>
   )
 }
+
+const LogoBox = styled(Box)`
+  width: 150px;
+  height: 150px;
+  background: gray;
+  margin: 20px 0px 0px 50px;
+`

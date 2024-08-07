08c2f89455a5feaf62227d6c19f5522e3b6b35fc
conconcc <lesj9217@gmail.com>
2024-08-07 19:01:12 +1000

    📝 docs : textfield label 한글로 일치하도록 수정

--- project/src/components/SignupTextField.jsx
+++ project/src/components/SignupTextField.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, TextField, InputAdornment, IconButton, Box, FormControl } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
-export function SignupTextField({ texttype, value, onChange }) {
+export function SignupTextField({ texttype,fieldname ,value, onChange }) {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6}>
@@ -10,9 +10,8 @@ export function SignupTextField({ texttype, value, onChange }) {
           autoComplete={texttype}
           name={texttype}
           required
-          fullWidth
           id={texttype}
-          label={texttype}
+          label={fieldname}
           value={value}
           onChange={onChange}
           autoFocus
@@ -22,8 +21,8 @@ export function SignupTextField({ texttype, value, onChange }) {
   );
 }
 
-export function PasswordField({ texttype, value, onChange }) {
-  const isPasswordField = () =>{texttype.toLowerCase().includes("password")};
+export function PasswordField({ texttype,fieldname, value, onChange,error,helperText }) {
+  const isPasswordField = () =>texttype.toLowerCase().includes("password");
   <Box
       component="form"
       noValidate
@@ -45,14 +44,15 @@ export function PasswordField({ texttype, value, onChange }) {
         <TextField
           margin="normal"
           required
-          fullWidth
           name={texttype}
-          label={texttype}
+          label={fieldname}
           type={isPasswordField ? "password" : "text"}
           id={texttype}
           autoComplete={isPasswordField ? 'current-password' : 'off'}
           value={value}
           onChange={onChange}
+          error={error}
+          helperText={helperText}
         />
       </Grid>
     </Grid>
@@ -60,7 +60,7 @@ export function PasswordField({ texttype, value, onChange }) {
 }
 
 
-export function SearchField({ texttype, value, onChange }) {
+export function SearchField({ texttype,fieldname, value, onChange }) {
   const handleSearchClick = () => {
     alert("아직 구현중인 기능입니다.");
   };
@@ -72,17 +72,16 @@ export function SearchField({ texttype, value, onChange }) {
           autoComplete={texttype}
           name={texttype}
           required
-          fullWidth
           id={texttype}
-          label={texttype}
+          label={fieldname}
           value={value}
           onChange={onChange}
           autoFocus
           InputProps={{
             endAdornment: (
               <InputAdornment position="end">
-                <IconButton>
-                  <SearchIcon onclick={handleSearchClick}/>
+                <IconButton onClick={handleSearchClick} universityName={value}>
+                  <SearchIcon />
                 </IconButton>
               </InputAdornment>
             ),

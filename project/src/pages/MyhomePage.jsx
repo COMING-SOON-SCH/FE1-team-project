c235c4ef0ca0287b2983377c403f9f10560f760e
conconcc <lesj9217@gmail.com>
2024-08-07 19:03:35 +1000

    📝 feat : 현재 비밀번호와 이전 비밀번호 비교 기능 추가

--- project/src/pages/MyhomePage.jsx
+++ project/src/pages/MyhomePage.jsx
@@ -1,107 +1,171 @@
-import React from 'react';
-import { Button, IconButton, Avatar, Box, Container, Stack } from '@mui/material';
-import SidebarIcon from '@mui/icons-material/Menu'; // 사이드바 아이콘
-import OptionsIcon from '@mui/icons-material/Settings'; // 옵션 아이콘
+import React, { useState } from 'react';
+import { Button, IconButton, Avatar, Container, Stack } from '@mui/material';
+import OptionsIcon from '@mui/icons-material/Settings';
+import { PasswordField, SignupTextField } from '../components/SignupTextField';
+import styled from 'styled-components';
 
+export function MyhomePage() {
+  const [name, setName] = useState('comingsoon');
+  const id='아이디';
+  const [password, setPassword] = useState('비밀번호');
+  const [currentPassword, setCurrentPassword] = useState('비밀번호');  
+  const [showPasswordChange, setShowPasswordChange] = useState(false);
 
-// 사이드바 버튼 컴포넌트
-function SidebarButton() {
+  const handlePasswordChangeClick = () => {
+    setShowPasswordChange(true);
+  };
+
+  const handleSave = () => {
+    if (password !== currentPassword) {
+      // 비밀번호가 다를 경우에만 저장
+      setCurrentPassword(password);
+      console.log('이름:', name);
+      console.log('비밀번호:', password);
+      alert('저장되었습니다.');
+    } else {
+      alert('새 비밀번호가 현재 비밀번호와 같습니다. 다른 비밀번호를 입력해주세요.');
+    }
+    setShowPasswordChange(false); 
+  };
+
   return (
-    <IconButton>
-      <SidebarIcon />
-    </IconButton>
-  );
-}
-
-// 옵션 버튼 컴포넌트
-function OptionsButton() {
-  return (
-    <IconButton>
-      <OptionsIcon />
-    </IconButton>
+    <Container sx={{ width: '100%', height: '100%' }}>
+      <BoxPosition>
+        <OptionsButton />
+      </BoxPosition>
+      <BoxCenter>
+        <ProfilePicture onUpload={handleUpload} />
+      </BoxCenter>
+      <ProfileInfo
+        name={name}
+        id={id}
+        password={password}
+        showPasswordChange={showPasswordChange}
+        setName={setName}
+        setPassword={setPassword}
+        onPasswordChangeClick={handlePasswordChangeClick}
+        onSave={handleSave}
+        currentPassword={currentPassword}
+      />
+    </Container>
   );
 }
 
 // 프로필 사진 컴포넌트
-function ProfilePicture({ onUpload }) {
+const ProfilePicture = ({ onUpload }) => {
   const handleFileChange = (event) => {
     // 파일 업로드 로직
     onUpload(event.target.files[0]);
   };
 
   return (
-    <Box>
-      <IconButton component="label">
-        <Avatar src="프로필 사진 URL" alt="프로필 사진" sx={{width : 150, height: 150, mt:15}}/>
-        <input type="file" hidden onChange={handleFileChange} />
-      </IconButton>
-    </Box>
+    <IconButton component="label">
+      <Avatar src="프로필 사진 URL" alt="프로필 사진" sx={{ width: 250, height: 250 }} />
+      <input type="file" hidden onChange={handleFileChange} />
+    </IconButton>
   );
-}
+};
 
 // 프로필 정보 컴포넌트
-function ProfileInfo({ name, id, password, onPasswordChange, onSave }) {
-    return (
-        <Box sx={{ width: '100%', textAlign: 'center' }}>
-        <Stack spacing={2} fontSize={20}>
-          <div>이름: {name}</div>
-          <div>아이디: {id}</div>
-          <div>비밀번호: <input type="password" value={password} /></div>
-          
-          <Box display="flex" flexDirection="column" alignItems="center" width="100%" marginTop={20}>  
-          <Button variant="contained" sx={{width:'35%'}} justifyContent='center' onClick={onPasswordChange}>
+const ProfileInfo = ({
+  name,
+  id,
+  password,
+  showPasswordChange,
+  setName,
+  setPassword,
+  onPasswordChangeClick,
+  onSave,
+  currentPassword
+}) => {
+  return (
+    <BoxWrapper>
+      <Stack spacing={4} fontSize={18}>
+        <BoxItem>
+          <Label>이름:</Label>
+          <SignupTextField texttype={'id'} fieldname={name} onChange={(e) => setName(e.target.value)} sx={{ flex: 1 }} />
+        </BoxItem>
+        <BoxItem>
+          <Label>아이디:</Label>
+          <div style={{ flex: 1 , textAlign:'left'}}>{id}</div>
+        </BoxItem>
+        {showPasswordChange && (
+          <div>
+          <BoxItem>
+            <Label> 현재 비밀번호:</Label>
+            <div style={{ flex: 1, textAlign:'left'}}><Label>{currentPassword}</Label></div>
+          </BoxItem>
+          <BoxItem>
+            <Label>변경 비밀번호:</Label>
+            <PasswordField texttype={'password'} fieldname={currentPassword} onChange={(e) => setPassword(e.target.value)} sx={{ flex: 1 }} />
+          </BoxItem>
+          </div>
+        )}
+        <BoxButtonContainer>
+          <Button variant="contained" sx={{ width: '35%' }} onClick={onPasswordChangeClick}>
             비밀번호 변경
           </Button>
-          <Button
-            variant="contained"
-            onClick={onSave}
-            sx={{ width: '20%', mt:2}} // 버튼 크기 조정
-            
-          >
+          <Button variant="contained" sx={{ width: '20%', mt: 2 }} onClick={onSave}>
             저장
           </Button>
-        </Box>
-        </Stack>
-      </Box>
-    );
-  }
-// 메인 컴포넌트
-export default function MyhomePage() {
-  const name='이름'
-  const id='아이디'
-  const password='비밀번호'
-
-  const handleUpload = (file) => {
-    // 사진 업로드 로직
-    console.log(file);
-  };
-
-  const handlePasswordChange = (event) => {
-    alert('아직 미구현입니다.');
-  };
-
-  const handleSave = () => {
-    // 저장 로직
-    console.log('저장되었습니다.');
-  };
-
-  return (
-    <Container sx={{width:'100%', margin :0}} >
-      <Box display="flex" justifyContent="space-between" mb={2}>
-        <SidebarButton />
-        <OptionsButton />
-      </Box>
-      <Box display="flex" justifyContent="center" mb={2}>
-        <ProfilePicture onUpload={handleUpload} />
-      </Box>
-      <ProfileInfo
-        name={name}
-        id={id}
-        password={password}
-        onPasswordChange={handlePasswordChange}
-        onSave={handleSave}
-      />
-    </Container>
+        </BoxButtonContainer>
+      </Stack>
+    </BoxWrapper>
   );
-}
+};
 
+// 옵션 버튼 컴포넌트
+const OptionsButton = () => {
+  return (
+    <IconButton>
+      <OptionsIcon sx={{ width: 40, height: 40 }} />
+    </IconButton>
+  );
+};
+
+// 사진 업로드 핸들러
+const handleUpload = (file) => {
+  // 사진 업로드 로직
+  console.log(file);
+};
+
+// 스타일드 컴포넌트 정의
+const BoxPosition = styled.div`
+  position: absolute;
+  top: 0;
+  right: 5px;
+`;
+
+const BoxCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20%;
+`;
+
+const BoxWrapper = styled.div`
+  width: 100%;
+  margin-top:20px;
+`;
+
+const BoxItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+const BoxButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 32px;
+`;
+
+const Label = styled.div`
+  width: 45%;
+  text-align : left;
+  
+`;
+
+export default MyhomePage;

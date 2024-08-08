b7083600c51411601ce7c2ccaa9a2eb14d7d1ba4
nay3on <nayeon0425s@gmail.com>
2024-08-08 18:58:49 +0900

    📝feat : 동아리 프로필 구성 내용 정리
    clubs 배열 생성

--- project/src/pages/ClubProfile.jsx
+++ project/src/pages/ClubProfile.jsx
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 import ClubProfileTabs from '../components/ClubProfileTabs';
 
+const clubs = [
+  {
+    ClubName: "",
+    Category: "",
+    ClubDescription: "",
+    ClubActivities: "",
+    ProfilePic: "",
+  },
+  {
+    ClubName: "썬시아",
+    Category: "예술",
+    ClubDescription: "순천향대학교 대표 치어리딩 동아리",
+    ClubActivities: "예상활동: 치어리딩, 축제 공연",
+    ProfilePic: "https://avatars.githubusercontent.com/u/141625960?v=4.png",
+  },
+  {
+    ClubName: "커밍순",
+    Category: "코딩",
+    ClubDescription: "순천향대학교 IT 서비스 개발 동아리",
+    ClubActivities: "스터디, 팀 프로젝트",
+    ProfilePic: "https://oopy.lazyrockets.com/api/v2/notion/image?src=https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F738ca975-1a2c-47b8-ae1f-4febc2c083b9%2Fd82c57e8-45ff-4394-8990-4722f1facf23%2FKakaoTalk_20240229_213126094.jpg&blockId=df28e650-90dd-4478-bce2-7596c1c41040&width=256",
+  },
+  {
+    ClubName: "리버티노",
+    Category: "예술",
+    ClubDescription: "오케스트라 동아리",
+    ClubActivities: "매달 정기 공연",
+    ProfilePic: "https://avatars.githubusercontent.com/u/175573826?s=200&v=4.png",
+  },
+];
+
 const ClubProfile = () => {
+  const [index, setIndex] = useState(1); // 불러올 동아리 프로필 인덱스 값
+  const { ClubName, Category, ClubDescription, ClubActivities, ProfilePic } = clubs[index];
+
   return (
     <Layout>
       <ProfileContainer>
         <TopArea>
-            <ProfilePic />
-            <ClubInfoContainer>
-              <ClubInfoTop>
-                <ClubName>썬시아</ClubName>
-                <Category>예술</Category>
-              </ClubInfoTop>
-              <ClubInfoBottom>
-                <ClubDescription>순천향대학교 대표 치어리딩 동아리</ClubDescription>
-                <ClubActivities>예상활동: 치어리딩, 축제 공연</ClubActivities>
-              </ClubInfoBottom>
-            </ClubInfoContainer>
+          <ProfilePicWrapper src={ProfilePic} alt="Profile Picture" />
+          <ClubInfoContainer>
+            <ClubInfoTop>
+              <ClubNameText>{ClubName}</ClubNameText>
+              <CategoryText>{Category}</CategoryText>
+            </ClubInfoTop>
+            <ClubInfoBottom>
+              <ClubDescriptionText>{ClubDescription}</ClubDescriptionText>
+              <ClubActivitiesText>{ClubActivities}</ClubActivitiesText>
+            </ClubInfoBottom>
+          </ClubInfoContainer>
         </TopArea>
-        <Tabs />
+        <ClubProfileTabs index={index} />
       </ProfileContainer>
     </Layout>
   );
-}
+};
 
 const ProfileContainer = styled.div`
   width: 393px;
@@ -41,7 +75,7 @@ const TopArea = styled.div`
   margin: auto;
 `;
 
-const ProfilePic = styled.img`
+const ProfilePicWrapper = styled.img`
   width: 120px;
   height: 120px;
   border-radius: 100%;
@@ -82,25 +116,25 @@ const ClubInfoBottom = styled.div`
   left: 154px;
 `;
 
-const ClubName = styled.div`
+const ClubNameText = styled.div`
   font-size: 20px;
   margin-left: 5px;
 `;
 
-const Category = styled.div`
+const CategoryText = styled.div`
   font-size: 14px;
   margin-top: 1px;
   margin-left: 5px;
   color: #666;
 `;
 
-const ClubDescription = styled.div`
+const ClubDescriptionText = styled.div`
   font-size: 12px;
   margin-top: 10px;
   text-align: left;
 `;
 
-const ClubActivities = styled.div`
+const ClubActivitiesText = styled.div`
   font-size: 12px;
   margin-top: 10px;
   text-align: left;

bd674a03b1d85dfc7841660e1c6d034f8cd5f4bc
Yusi <yusi4781@naver.com>
2024-08-10 04:01:39 +0900

    📝feat : 체크박스 기능 구현

--- project/src/components/PlanPopup.jsx
+++ project/src/components/PlanPopup.jsx
@@ -1,32 +1,45 @@
-import { Box, Button, Modal, TextField } from '@mui/material';
+import { Box, Button, Checkbox, Modal, TextField } from '@mui/material';
 import { Timestamp } from 'firebase/firestore';
 import React, { useState } from 'react';
 import styled, { keyframes } from "styled-components";
 import usePostPlan from '../hooks/usePostPlan';
+import useFetchPlanState from '../hooks/useFetchPlanState';
 
 export default function PlanPopup({ closePopup, data, showPopup, selectedTime }) {
   const [isClicked, setIsClicked] = useState(false);
   const [planToAdd, setPlanToAdd] = useState("");
   const { postPlan, isError } = usePostPlan()
+  const fetchPlanState = useFetchPlanState()
 
   const handleAddPlan = (e) => {
-    const dateParts = selectedTime.split('-');
-    const year = parseInt(dateParts[0], 10);
-    const month = parseInt(dateParts[1], 10) - 1; // 월은 0부터 시작하므로 1을 빼야 함
-    const day = parseInt(dateParts[2], 10);
-    const date = new Date(year, month, day);
-    const firebaseTimestamp = Timestamp.fromDate(date);
+    if (planToAdd !== '') {
+      const dateParts = selectedTime.split('-');
+      const year = parseInt(dateParts[0], 10);
+      const month = parseInt(dateParts[1], 10) - 1; // 월은 0부터 시작하므로 1을 빼야 함
+      const day = parseInt(dateParts[2], 10);
+      const date = new Date(year, month, day);
+      const firebaseTimestamp = Timestamp.fromDate(date);
 
-    const planData = {
-      date: firebaseTimestamp,
-      plans: planToAdd
+      const planData = {
+        date: firebaseTimestamp,
+        plans: planToAdd,
+        state: false,
+      }
+
+      postPlan(planData);
+      data.push(planData);
+      if (isError) alert('데이터베이스에 계획 추가 중 오류 발생');
     }
 
-    postPlan(planData);
-    data.push(planData);
     setIsClicked(false);
-    if (isError) alert('데이터베이스에 계획 추가 중 오류 발생');
+    setPlanToAdd('');
   };
+
+  const handleCheckboxChange = (plan) => {
+    plan.state = !plan.state;
+    setPlanToAdd(prev => prev + 1)
+    fetchPlanState(plan)
+  }
 
   return (
     <Modal
@@ -36,51 +49,36 @@ export default function PlanPopup({ closePopup, data, showPopup, selectedTime })
       aria-describedby="modal-modal-description"
     >
       <PopupContainer>
-        {data.length ?
-          <div>
-            <div>
-              <Time>{selectedTime}</Time>
-              {
-                data.map((data) => (
-                  <Title key={data.plans}>{data.plans}</Title>
-                ))
-              }
-            </div>
-            <AnimatedContainer isClicked={isClicked}>
-              {!isClicked ? (
-                <AddButton variant="outlined" onClick={() => setIsClicked(true)}>+</AddButton>
-              ) : (
-                <InputContainer>
-                  <StyledTextField
-                    value={planToAdd}
-                    onChange={(e) => setPlanToAdd(e.target.value)}
-                    autoFocus
-                  />
-                  <AddPlanButton variant="contained" onClick={handleAddPlan}>추가</AddPlanButton>
-                </InputContainer>
-              )}
-            </AnimatedContainer>
-          </div>
-          :
-          <div>
-            <Time>{selectedTime}</Time>
-            <Title>No Plan</Title>
-            <AnimatedContainer isClicked={isClicked}>
-              {!isClicked ? (
-                <AddButton variant="outlined" onClick={() => setIsClicked(true)}>+</AddButton>
-              ) : (
-                <InputContainer>
-                  <StyledTextField
-                    value={planToAdd}
-                    onChange={(e) => setPlanToAdd(e.target.value)}
-                    autoFocus
-                  />
-                  <AddPlanButton variant="contained" onClick={handleAddPlan}>추가</AddPlanButton>
-                </InputContainer>
-              )}
-            </AnimatedContainer>
-          </div>
-        }
+        <ContentsBox>
+          <Time>{selectedTime}</Time>
+          {
+            data.length ? data.map((plan) => (
+              <PlanContainer key={plan.plans}>
+                <Checkbox
+                  checked={plan.state}
+                  onChange={() => handleCheckboxChange(plan)}
+                />
+                <Plan key={data.plans} complted={plan.state}>{plan.plans}</Plan>
+              </PlanContainer>
+            ))
+              : <PlanContainer>
+                <Plan>No Plan</Plan>
+              </PlanContainer>
+          }
+        </ContentsBox>
+        <AnimatedContainer isClicked={isClicked}>
+          {!isClicked ? (
+            <AddButton variant="outlined" onClick={() => setIsClicked(true)}>+</AddButton>
+          ) : (
+            <InputContainer>
+              <StyledTextField
+                onChange={(e) => setPlanToAdd(e.target.value)}
+                autoFocus
+              />
+              <AddPlanButton variant="contained" onClick={handleAddPlan}>추가</AddPlanButton>
+            </InputContainer>
+          )}
+        </AnimatedContainer>
       </PopupContainer>
     </Modal>
   )
@@ -102,6 +100,13 @@ const PopupContainer = styled(Box)`
   justify-content: center;
 `
 
+const ContentsBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
 const Time = styled.p`
   color: #666;
   font-size: 0.9rem;
@@ -109,8 +114,15 @@ const Time = styled.p`
   padding: 20px 0px 0px 0px;
 `;
 
-const Title = styled.h2`
-  padding: 0px 0px 0px 0px;
+const PlanContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 15px 0px 0px 0px;
+`;
+
+const Plan = styled.h2`
+  margin: 0;
+  text-decoration: ${props => props.complted ? 'line-through' : 'none'};
 `;
 
 const expandAnimation = keyframes`
@@ -130,7 +142,7 @@ const AnimatedContainer = styled.div`
   align-items: center;
   transition: width 0.3s ease-in-out;
   animation: ${props => props.isClicked ? expandAnimation : 'none'} 1s ease-in-out;
-  margin-bottom: 10px;
+  margin: 15px;
 `;
 
 const InputContainer = styled.div`
